1de204cf03fbdc6598872a545c57e3c34f8644b4
Sergey Shilovskiy <sergey.shilovskiy@immersve.com>
2025-01-30 17:50:59 +1300

    revert - for workshop

--- main.go
+++ main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 	"go/crypto/api"
+	"sync"
 )
 
-func main() {}
+func main() {
+	currencies := []string {"BTC", "ETH", "USDC"}
+
+	var wg sync.WaitGroup
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func() {
+			getCurrencyRate(currency)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
 
 func getCurrencyRate(currency string) {
 	rate, err := api.GetRate(currency)
